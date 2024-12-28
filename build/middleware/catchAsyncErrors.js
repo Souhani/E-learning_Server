7c67b4711147ffa8f276641f685cbd86665a5de1
Souhani <souhanikarim1999@gmail.com>
2024-12-28 20:21:13 +0100

    ts config

--- build/middleware/catchAsyncErrors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsyncErrors = void 0;
-const catchAsyncErrors = (myAsyncFunc) => (req, res, next) => {
-    Promise.resolve(myAsyncFunc(req, res, next)).catch((err) => next(err));
-};
-exports.catchAsyncErrors = catchAsyncErrors;
