47b585e70ef62bd6d3410e173778eef36fa63b78
Souhani <souhanikarim1999@gmail.com>
2024-12-28 16:27:11 +0100

    deployment error

--- routes/quiz.route.ts
+++ routes/quiz.route.ts
@@ -1,20 +1,21 @@
 import { Router } from "express";
 import { deleteQuizById, getAllQuizzes, getQuizByCourseId, uploadQuiz } from "../controllers/quiz.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 
 const quizRouter = Router();
 
 // create quiz route: (protected Admin route).
-quizRouter.post("/create-quiz", uploadQuiz);
+quizRouter.post("/create-quiz", isAuthenticated, authorizeRoles("Admin"), uploadQuiz);
 
 // get quiz route
-quizRouter.get("/get-quiz/:course_id", getQuizByCourseId);
+quizRouter.get("/get-quiz/:course_id", isAuthenticated, getQuizByCourseId);
 
 // get all quizzes route: (protected Admin route).
-quizRouter.get("/get-all-quizzes", getAllQuizzes);
+quizRouter.get("/get-all-quizzes", isAuthenticated, authorizeRoles("Admin"),  getAllQuizzes);
 
 // delete quiz route: (protected Admin route).
-quizRouter.delete("/delete-quiz/:id", deleteQuizById);
+quizRouter.delete("/delete-quiz/:id",isAuthenticated, authorizeRoles("Admin"),  deleteQuizById);
 
 
 export default quizRouter;
